74b28b7875e6e7519d131c17aade1b2c67af60ae
Choisungwook <kgg1959@naver.com>
2023-08-13 21:29:26 +0900

    workflow 연관관계 테스트

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-# resource "aws_vpc" "main" {
-#   cidr_block = "192.168.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.0.0/16"
 
-#   tags = {
-#     Name = "terraform VPC"
-#   }
-# }
+  tags = {
+    Name = "terraform VPC"
+  }
+}
